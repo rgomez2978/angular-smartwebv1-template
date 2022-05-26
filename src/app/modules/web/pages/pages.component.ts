2453b84e527568a0e2c63215bd81c62e925fae57
Ronald Gomez <rgomez2978@gmail.com>
2022-05-26 00:05:53 -0400

    initial commit

--- src/app/modules/web/pages/pages.component.ts
+++ src/app/modules/web/pages/pages.component.ts
@@ -31,45 +31,45 @@ export class PagesComponent implements OnInit {
               {
                 nav_id: 1,
                 name: 'Inicio',
-                link: './home',
+                link: '/home',
                 icon: 'fas fa-home',
                 order: 1
               },
               {
                 nav_id: 2,
                 name: 'Nosotros',
-                link: './about',
+                link: '/about',
                 icon: 'fas fa-address-card',
                 order: 2
               },
               {
                 nav_id: 3,
                 name: 'Productos',
-                link: './products',
+                link: '/products',
                 icon: 'fab fa-artstation',
                 order: 3
               }, {
                 nav_id: 4,
                 name: 'Clientes',
-                link: './clients',
+                link: '/clients',
                 icon: 'fas fa-user-tag',
                 order: 4
               }, {
                 nav_id: 5,
                 name: 'Aliados',
-                link: './partners',
+                link: '/partners',
                 icon: 'fas fa-people-arrows',
                 order: 5
               }, {
                 nav_id: 6,
                 name: 'Noticias',
-                link: './news',
+                link: '/news',
                 icon: 'far fa-newspaper',
                 order: 6
               }, {
                 nav_id: 7,
                 name: 'Contactos',
-                link: './contacts',
+                link: '/contacts',
                 icon: 'fas fa-envelope-open-text',
                 order: 7
               },
@@ -127,25 +127,25 @@ export class PagesComponent implements OnInit {
               {
                 fo_id: 1,
                 description: 'Políticas de privacidad',
-                link: './privacy',
+                link: '/privacy',
                 order: 1,
               },
               {
                 fo_id: 2,
                 description: 'Términos legales',
-                link: './terms',
+                link: '/terms',
                 order: 2,
               },
               {
                 fo_id: 3,
                 description: 'Sitios relacionados',
-                link: './related',
+                link: '/related',
                 order: 3,
               },
               {
                 fo_id: 4,
                 description: 'Mapa del sitio',
-                link: './sitemap',
+                link: '/sitemap',
                 order: 4,
               },
             ]
